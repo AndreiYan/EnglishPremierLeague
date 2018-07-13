ef350b53d91b7835c7a7e3b1f5cabf184ddd687a
AndreiYan <Aandreiyan@yahoo.co.uk>
2018-07-12 23:09:18 -0400

    one submit button

--- app/src/main/java/com/example/android/englishpremierleague/Page2Activity.java
+++ app/src/main/java/com/example/android/englishpremierleague/Page2Activity.java
@@ -16,15 +16,16 @@ public class Page2Activity extends AppCompatActivity {
     String theName;
     int score = 0;
 
-
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
         configureStartButton();
-        setSubmit2Button();
-        setSubmit3Button();
-        setSubmit4Button();
+//        setSubmit2Button();
+//        setSubmit3Button();
+//        setSubmit4Button();
 
         Bundle extras = getIntent().getExtras();
         String name2 = extras.getString("QuizName");
@@ -32,56 +33,56 @@ public class Page2Activity extends AppCompatActivity {
     }
 
 
-
-    public void setSubmit2Button() {
-
-        Button submit2Button = findViewById(R.id.submit2);
-        submit2Button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                RadioGroup radioGroup = findViewById(R.id.RadioGrouptotts);
-                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-                if (checkedRadioButtonId == 3) {
-                    score = score + 3;
-                }
-
-            }
-
-
-        });
-
-    }
-
-    public void setSubmit3Button() {
-
-        Button submit3Button = findViewById(R.id.submit3);
-        submit3Button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View view) {
-                EditText name = findViewById(R.id.manager_Name);
-                String managersName = name.getText().toString();
-                if (name != null && managersName.equals("Wenger")) {
-                    score = score + 3;
-                } else {
-                    Toast.makeText(Page2Activity.this, "Please enter the Last Name", Toast.LENGTH_SHORT).show();
-
-
-                }
-
-            }
-
-
-        });
-
-
-    }
-
-    public void setSubmit4Button() {
-
-        Button submit4Button = findViewById(R.id.submit4);
-        submit4Button.setOnClickListener(new View.OnClickListener() {
-
+//
+//    public void setSubmit2Button() {
+//
+//        Button submit2Button = findViewById(R.id.submit2);
+//        submit2Button.setOnClickListener(new View.OnClickListener() {
+//
 //            public void onClick(View view) {
+//                RadioGroup radioGroup = findViewById(R.id.RadioGrouptotts);
+//                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
+//                if (checkedRadioButtonId == 3) {
+//                    score = score + 3;
+//                }
+//
+//            }
+//
+//
+//        });
+//
+//    }
+
+//    public void setSubmit3Button() {
+//
+//        Button submit3Button = findViewById(R.id.submit3);
+//        submit3Button.setOnClickListener(new View.OnClickListener() {
+//
+//            public void onClick(View view) {
+//                EditText name = findViewById(R.id.manager_Name);
+//                String managersName = name.getText().toString();
+//                if (name != null && managersName.equals("Wenger")) {
+//                    score = score + 3;
+//                } else {
+//                    Toast.makeText(Page2Activity.this, "Please enter the Last Name", Toast.LENGTH_SHORT).show();
+//
+//
+//                }
+//
+//            }
+//
+//
+//        });
+//
+//
+//    }
+
+//    public void setSubmit4Button() {
+//
+//        Button submit4Button = findViewById(R.id.submit4);
+//        submit4Button.setOnClickListener(new View.OnClickListener() {
+//
+//           public void onClick(View view) {
 //                Ch1 = (CheckBox) findViewById(R.id.checkboxArsenal);
 //                Ch2 = (CheckBox) findViewById(R.id.checkboxChelsea);
 //                Ch3 = (CheckBox) findViewById(R.id.checkboxEverton);
@@ -91,82 +92,117 @@ public class Page2Activity extends AppCompatActivity {
 //                  If(checkboxArsenal.isChecked&&checkboxChelsea.isChecked&&
 //                      checkboxEverton.isChecked&&checkboxLiverpool.isChecked&&
 //                      checkboxMunt.isChecked&&checkboxTotts.isChecked){
-                        score = score +1;
-                    else
-                        score = score + 0;
+//                        score = score +1;
+//                    else
+//                        score = score + 0;
 //
 //                  }
 //            }
 
-            public void onClick(View view) {
-                boolean checked = ((CheckBox) view).isChecked();
-                switch (view.getId()) {
-                    case R.id.checkboxArsenal:
-                        if (checked)
-                            score = score + 1;
-                        else
-                            score = score + 0;
-                            break;
-
-                    case R.id.checkboxChelsea:
-                        if (checked)
-                            score = score + 1;
-                        else
-                            score = score + 0;
-                        break;
-
-
-                    case R.id.checkboxEverton:
-                        if (checked)
-                            score = score + 1;
-                        else
-                            score = score + 0;
-                        break;
-
-                    case R.id.checkboxLiverpool:
-                        if (checked)
-                            score = score + 1;
-                        else
-                            score = score + 0;
-
-                        break;
-                    case R.id.checkboxMunt:
-                        if (checked)
-                            score = score + 1;
-                        else
-                            score = score + 0;
-
-                        break;
-                    case R.id.checkboxTotts:
-                        if (checked)
-                            score = score + 1;
-                        else
-                            score = score + 0;
-
-                        break;
-
-                }
-
-
-           }
-
-
-        });
-
-
-    }
+//            public void onClick(View view) {
+//                boolean checked = ((CheckBox) view).isChecked();
+//                switch (view.getId()) {
+//                    case R.id.checkboxArsenal:
+//                        if (checked)
+//                            score = score + 1;
+//                        else
+//                            score = score + 0;
+//                            break;
+//
+//                    case R.id.checkboxChelsea:
+//                        if (checked)
+//                            score = score + 1;
+//                        else
+//                            score = score + 0;
+//                        break;
+//
+//
+//                    case R.id.checkboxEverton:
+//                        if (checked)
+//                            score = score + 1;
+//                        else
+//                            score = score + 0;
+//                        break;
+//
+//                    case R.id.checkboxLiverpool:
+//                        if (checked)
+//                            score = score + 1;
+//                        else
+//                            score = score + 0;
+//
+//                        break;
+//                    case R.id.checkboxMunt:
+//                        if (checked)
+//                            score = score + 1;
+//                        else
+//                            score = score + 0;
+//
+//                        break;
+//                    case R.id.checkboxTotts:
+//                        if (checked)
+//                            score = score + 1;
+//                        else
+//                            score = score + 0;
+//
+//                        break;
+//
+//                }
+//
+//
+//           }
+//
+//
+//        });
+//
+//
+//    }
 
     public void configureStartButton() {
 
         Button startButton = findViewById(R.id.score);
         startButton.setOnClickListener(new View.OnClickListener() {
 
+
             public void onClick(View view) {
-                RadioGroup radioGroup = findViewById(R.id.RadioGroupMut);
-                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-                if (checkedRadioButtonId == 1) {
+
+                RadioGroup radioGroup2 = findViewById(R.id.RadioGroupMut);
+                int checkedRadioButtonId2 = radioGroup2.getCheckedRadioButtonId();
+                if (checkedRadioButtonId2 == 1) {
                     score = score + 3;
                 }
+
+                RadioGroup radioGroup1 = findViewById(R.id.RadioGrouptotts);
+                int checkedRadioButtonId1 = radioGroup1.getCheckedRadioButtonId();
+                if (checkedRadioButtonId1 == 3) {
+                    score = score + 3;
+                }
+
+                EditText name = findViewById(R.id.manager_Name);
+                String managersName = name.getText().toString();
+                if (name != null && managersName.equals("Wenger")) {
+                    score = score + 3;
+                } else {
+                    Toast.makeText(Page2Activity.this, "Please enter the Last Name", Toast.LENGTH_SHORT).show();
+
+
+                }
+                Ch1 = (CheckBox) findViewById(R.id.checkboxArsenal);
+                Ch2 = (CheckBox) findViewById(R.id.checkboxChelsea);
+                Ch3 = (CheckBox) findViewById(R.id.checkboxEverton);
+                Ch4 = (CheckBox) findViewById(R.id.checkboxLiverpool);
+                Ch5 = (CheckBox) findViewById(R.id.checkboxMunt);
+                Ch6 = (CheckBox) findViewById(R.id.checkboxTotts);
+
+
+                if (Ch1.isChecked && checkboxChelsea.isChecked &&
+                        checkboxEverton.isChecked && checkboxLiverpool.isChecked &&
+                        checkboxMunt.isChecked && checkboxTotts.isChecked) {
+                    score = score + 3;
+                } else {
+                    score = score + 0;
+
+                }
+
 
                 String finalName = theName;
                 String finalScore = Integer.toString(score);
