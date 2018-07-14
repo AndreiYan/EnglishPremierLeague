e6cc1f5d8ae307f32dfd9c22f55597abe41fe020
AndreiYan <Aandreiyan@yahoo.co.uk>
2018-07-13 23:58:35 -0400

    UPdated check boxes

--- app/src/main/java/com/example/android/englishpremierleague/Page2Activity.java
+++ app/src/main/java/com/example/android/englishpremierleague/Page2Activity.java
@@ -15,6 +15,8 @@ import android.widget.Toast;
 public class Page2Activity extends AppCompatActivity {
     String theName;
     int score = 0;
+    CheckBox Ch1, Ch2, Ch3, Ch4, Ch5, Ch6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,140 +24,19 @@ public class Page2Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_page2);
 
+        Ch1 = findViewById(R.id.checkboxArsenal);
+        Ch2 = findViewById(R.id.checkboxChelsea);
+        Ch3 = findViewById(R.id.checkboxEverton);
+        Ch4 = findViewById(R.id.checkboxLiverpool);
+        Ch5 = findViewById(R.id.checkboxMunt);
+        Ch6 = findViewById(R.id.checkboxTotts);
+
         configureStartButton();
-//        setSubmit2Button();
-//        setSubmit3Button();
-//        setSubmit4Button();
 
         Bundle extras = getIntent().getExtras();
         String name2 = extras.getString("QuizName");
         theName = name2;
     }
-
-
-//
-//    public void setSubmit2Button() {
-//
-//        Button submit2Button = findViewById(R.id.submit2);
-//        submit2Button.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View view) {
-//                RadioGroup radioGroup = findViewById(R.id.RadioGrouptotts);
-//                int checkedRadioButtonId = radioGroup.getCheckedRadioButtonId();
-//                if (checkedRadioButtonId == 3) {
-//                    score = score + 3;
-//                }
-//
-//            }
-//
-//
-//        });
-//
-//    }
-
-//    public void setSubmit3Button() {
-//
-//        Button submit3Button = findViewById(R.id.submit3);
-//        submit3Button.setOnClickListener(new View.OnClickListener() {
-//
-//            public void onClick(View view) {
-//                EditText name = findViewById(R.id.manager_Name);
-//                String managersName = name.getText().toString();
-//                if (name != null && managersName.equals("Wenger")) {
-//                    score = score + 3;
-//                } else {
-//                    Toast.makeText(Page2Activity.this, "Please enter the Last Name", Toast.LENGTH_SHORT).show();
-//
-//
-//                }
-//
-//            }
-//
-//
-//        });
-//
-//
-//    }
-
-//    public void setSubmit4Button() {
-//
-//        Button submit4Button = findViewById(R.id.submit4);
-//        submit4Button.setOnClickListener(new View.OnClickListener() {
-//
-//           public void onClick(View view) {
-//                Ch1 = (CheckBox) findViewById(R.id.checkboxArsenal);
-//                Ch2 = (CheckBox) findViewById(R.id.checkboxChelsea);
-//                Ch3 = (CheckBox) findViewById(R.id.checkboxEverton);
-//                Ch4 = (CheckBox) findViewById(R.id.checkboxLiverpool);
-//                Ch5 = (CheckBox) findViewById(R.id.checkboxMunt);
-//                Ch6 = (CheckBox) findViewById(R.id.checkboxTotts);
-//                  If(checkboxArsenal.isChecked&&checkboxChelsea.isChecked&&
-//                      checkboxEverton.isChecked&&checkboxLiverpool.isChecked&&
-//                      checkboxMunt.isChecked&&checkboxTotts.isChecked){
-//                        score = score +1;
-//                    else
-//                        score = score + 0;
-//
-//                  }
-//            }
-
-//            public void onClick(View view) {
-//                boolean checked = ((CheckBox) view).isChecked();
-//                switch (view.getId()) {
-//                    case R.id.checkboxArsenal:
-//                        if (checked)
-//                            score = score + 1;
-//                        else
-//                            score = score + 0;
-//                            break;
-//
-//                    case R.id.checkboxChelsea:
-//                        if (checked)
-//                            score = score + 1;
-//                        else
-//                            score = score + 0;
-//                        break;
-//
-//
-//                    case R.id.checkboxEverton:
-//                        if (checked)
-//                            score = score + 1;
-//                        else
-//                            score = score + 0;
-//                        break;
-//
-//                    case R.id.checkboxLiverpool:
-//                        if (checked)
-//                            score = score + 1;
-//                        else
-//                            score = score + 0;
-//
-//                        break;
-//                    case R.id.checkboxMunt:
-//                        if (checked)
-//                            score = score + 1;
-//                        else
-//                            score = score + 0;
-//
-//                        break;
-//                    case R.id.checkboxTotts:
-//                        if (checked)
-//                            score = score + 1;
-//                        else
-//                            score = score + 0;
-//
-//                        break;
-//
-//                }
-//
-//
-//           }
-//
-//
-//        });
-//
-//
-//    }
 
     public void configureStartButton() {
 
@@ -184,19 +65,12 @@ public class Page2Activity extends AppCompatActivity {
                 } else {
                     Toast.makeText(Page2Activity.this, "Please enter the Last Name", Toast.LENGTH_SHORT).show();
 
-
                 }
-                Ch1 = (CheckBox) findViewById(R.id.checkboxArsenal);
-                Ch2 = (CheckBox) findViewById(R.id.checkboxChelsea);
-                Ch3 = (CheckBox) findViewById(R.id.checkboxEverton);
-                Ch4 = (CheckBox) findViewById(R.id.checkboxLiverpool);
-                Ch5 = (CheckBox) findViewById(R.id.checkboxMunt);
-                Ch6 = (CheckBox) findViewById(R.id.checkboxTotts);
 
 
-                if (Ch1.isChecked && checkboxChelsea.isChecked &&
-                        checkboxEverton.isChecked && checkboxLiverpool.isChecked &&
-                        checkboxMunt.isChecked && checkboxTotts.isChecked) {
+                if (Ch1.isChecked() && Ch2.isChecked() &&
+                        Ch3.isChecked() && Ch4.isChecked() &&
+                        Ch5.isChecked() && Ch6.isChecked()) {
                     score = score + 3;
                 } else {
                     score = score + 0;
